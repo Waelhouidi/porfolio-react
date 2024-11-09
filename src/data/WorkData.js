1f768832e7f230390cf33ce13df0a8de940d0dce
Waelhouidi <waelhwidi0@gmail.com>
2024-11-09 15:10:06 +0100

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,35 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Agency Landing Page",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Job listing website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","sass"],
+        
+        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Worder Game ",
+        description:"Its a game created in JavaScript ",
+        tags:["JavaScript","localStorage","sass"],
+        
+        demo:"none",
+        github:"https://github.com/Waelhouidi/chekWordGame.git"
+    },
+   
+    
+  
+] 
